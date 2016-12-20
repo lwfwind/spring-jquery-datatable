020670bb6b75b73aa133d4e14c3dc76c96098f03
wind <wind@abc360.com>
2016-12-20 17:11:59 +0800

    fix bug

--- src/com/web/spring/datatable/TableQuery.java
+++ src/com/web/spring/datatable/TableQuery.java
@@ -40,7 +40,12 @@ public class TableQuery {
         this.entiteClass = entiteClass;
         this.criterias = criterias;
         this.customSQL = customSQL;
-        List<String> columnList = Arrays.asList(StringHelper.getBetweenString(this.customSQL.toLowerCase(), "select", "from ").split(","));
+        List<String> columnList;
+        if(this.customSQL.toLowerCase().contains("\nfrom ")){
+            columnList = Arrays.asList(StringHelper.getBetweenString(this.customSQL.toLowerCase(), "select", "\nfrom ").split(","));
+        }else {
+            columnList = Arrays.asList(StringHelper.getBetweenString(this.customSQL.toLowerCase(), "select", " from ").split(","));
+        }
         for (String columnName : columnList) {
             if (columnName.toLowerCase().contains(" as ")) {
                 selectColumnList.add(columnName.substring(columnName.lastIndexOf("as") + 2).trim());
